2b83c8981ed6c73932f75402cdb94734e5e00952
sitatec <sita.berete.3@gmail.com>
2021-03-12 02:31:48 -0500

    Add toolbar menu

--- app/src/main/java/com/sitatech/mareu/MeetingsListActivity.java
+++ app/src/main/java/com/sitatech/mareu/MeetingsListActivity.java
@@ -1,8 +1,11 @@
 package com.sitatech.mareu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.sitatech.mareu.databinding.ActivityMeetingListBinding;
 
@@ -15,5 +18,18 @@ public class MeetingsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMeetingListBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // It's not necessary to check the item id because there is only one menu item.
+
+        return super.onOptionsItemSelected(item);
     }
 }
